01196c87480625e51e584ff6b2dfc9ca44798c76
Zach Starman <starman.zach@gmail.com>
2024-08-02 14:40:49 -0500

    Add verbose arguments for main.cpp (added options to be called with makefile as well)

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,10 +1,30 @@
+#include <sstream>
 #include <iostream>
-#include <fluids_simulation.h>
-//#include <fluid_mechanics.h>
+#include "fluids_simulation.h"
 
 int main(int argc, char* argv[]) {
-	
+	bool verbose = false;
+
+	for(int i = 0; i < argc; i++) {
+		char* cmd = argv[i];
+		if(cmd[0] == '-') {
+			std::stringstream ss;
+			switch(cmd[1]) {
+				case 'v':
+					ss << argv[++i];
+					if(!(ss >> std::boolalpha >> verbose)) {
+						std::cout << "Incorrect argument" << std::endl;
+					}
+					break;
+
+				default:;
+			}
+		}
+	}
 	fluid_sim_2D test;
+	if(verbose) {
+		test.print_inputs();
+	}
 
 	return 0; 
 }
